ebca81a0dbe263a4952794eb1b40316e341f7d00
HeKaiLang <115347950+HeKaiLang@users.noreply.github.com>
2023-07-23 20:17:35 +0800

    简单修改（类继承改进）

---
+++ ws00_helloworld/build/pkg01_helloworld_cpp/CMakeFiles/helloworld2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/helloworld2.dir/src/helloworld2.cpp.o"
+  "CMakeFiles/helloworld2.dir/src/helloworld2.cpp.o.d"
+  "helloworld2"
+  "helloworld2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/helloworld2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
